eb37c41b8d654cb1fa25ada946a9bc72ec3a47d1
vlarusweb <vlarusweb@gmail.com>
2023-11-02 00:19:27 +0200

    feat: added complete template

---
+++ Projects50days/1ExpandingCard/script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        //removeActiveClasses();
+        panels.forEach(panel => {
+            panel.classList.remove('active');
+        })
+        panel.classList.add('active');
+    })
+})
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active');
+//     })
+// }
